6228344a7359ab7ff5a116d0b0030dfa075cddbb
Serhii <serghalak@gmail.com>
2026-09-16 17:51:05 +0300

    add git hub and facebook client registrations to security config

--- src/main/java/com/eazybytes/config/SecurityConfig.java
+++ src/main/java/com/eazybytes/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -11,27 +15,35 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .anyRequest().authenticated()
-//                )
-//                // Вмикаємо OAuth2 автентифікацію замість стандартного Form Login
-//                .oauth2Login(Customizer.withDefaults());
-//
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/secure").authenticated()
+                        .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
 
-//    @Bean
-//    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .anyRequest().authenticated()
-//                )
-//                // Вмикаємо OAuth2 автентифікацію замість стандартного Form Login
-//                .oauth2Login(Customizer.withDefaults());
-//
-//        return http.build();
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration gitHub = gitHubClientRegistration();
+        //ClientRegistration facebook = facebookHubClientRegistration();
+        return new InMemoryClientRegistrationRepository(gitHub/*, facebook*/);
+    }
+
+    private ClientRegistration gitHubClientRegistration() {
+        return CommonOAuth2Provider.GITHUB.getBuilder("github")
+                .clientId("Iv23liB5tSX4dc2FRVRp")
+                .clientSecret("d52cf372aa778601a2eafb2a8ede09e9da10f9e3")
+                .build();
+    }
+
+//    private ClientRegistration facebookHubClientRegistration() {
+//        return CommonOAuth2Provider.FACEBOOK.getBuilder("facebook")
+//                .clientId("facebook-client-id")
+//                .clientSecret("facebook-client-secret")
+//                .build();
 //    }
 }
